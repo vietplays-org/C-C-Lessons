fc328e3a8b0df1cf800f49e70ad4b8104047fd8a
vietplays <vietplaysvn@gmail.com>
2017-03-24 11:24:49 +0700

    lesson 5 finished

--- lesson5-sum/lesson5-sum/main.cpp
+++ lesson5-sum/lesson5-sum/main.cpp
@@ -7,9 +7,22 @@
 //
 
 #include <iostream>
+#include <iomanip>
+using namespace std;
 
+float sum5(int n){
+    float S = 0;
+    for(int i = 0; i <= n; i++){
+        S += 1.0/(2*i+1);
+    }
+    return S;
+}
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    int n;
+    float S = 0;
+    cout << "Input n = "; cin >> n;
+    S = sum5(n);
+    cout << "S = " << S << "\n";
+    cout << fixed << setprecision(2) << "S = " << S << "\n";
     return 0;
 }
